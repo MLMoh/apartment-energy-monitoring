97eae66d3d2623f99c9a78732f592c9af43e7ca6
MLMoh <157794932+MLMoh@users.noreply.github.com>
2025-04-07 18:26:57 +0100

    Create iot.tf

---
+++ terraform/iot.tf
@@ -0,0 +1,11 @@
+resource "aws_iot_topic_rule" "process_energy_data" {
+  name        = "ProcessEnergyDataRule"
+  description = "Route incoming IoT messages to Lambda function"
+  enabled     = true
+  sql         = "SELECT * FROM 'energy/topic'"
+  sql_version = "2016-03-23"
+
+  lambda {
+    function_arn = aws_lambda_function.process_energy_data.arn
+  }
+}
